fc87bbe959b8aee625577bb08d845524c0579b8d
Beth <betzabeth290104@gmail.com>
2026-07-13 17:41:35 -0500

    Corregir url base de axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ugestion-backend.onrender.com/api' 
+    baseURL: 'https://ugestion-backend.onrender.com' 
 });
 
 export default api;
